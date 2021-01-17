6fda04d1c2a5005842dc8a9ac8b8b2c607a912df
ravindra281298 <ravindra281298@gmail.com>
2021-01-17 18:17:35 +0530

    final commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = (props) => {
+    return (
+        <div>
+            <img src="https://cdncontribute.geeksforgeeks.org/wp-content/uploads/404.png" alt="404 Page Not Found" />
+        </div>
+    )
+}
+
+export default NotFound;
